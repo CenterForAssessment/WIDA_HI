df3e55fc06ad080bdc7251fe86bd73b7534daaf0
Damian Betebenner <dbetebenner@nciea.org>
2021-06-07 04:19:24 -0400

    Update with 2021 SGP scripts

---
+++ WIDA_HI_SGP_2021.R
@@ -0,0 +1,39 @@
+##########################################################################################
+###
+### Script for calculating SGPs for 2020-2021 WIDA/ACCESS Indiana
+###
+##########################################################################################
+
+### Load SGP package
+
+require(SGP)
+
+
+### Load Data
+
+load("Data/WIDA_HI_SGP.Rdata")
+load("Data/WIDA_HI_Data_LONG_2021.Rdata")
+
+
+### Run analyses
+
+WIDA_HI_SGP <- updateSGP(
+		WIDA_HI_SGP,
+		WIDA_HI_Data_LONG_2021,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "visualizeSGP", "outputSGP"),
+		sgp.percentiles=TRUE,
+		sgp.projections=TRUE,
+		sgp.projections.lagged=TRUE,
+		sgp.percentiles.baseline=FALSE,
+		sgp.projections.baseline=FALSE,
+		sgp.projections.lagged.baseline=FALSE,
+		get.cohort.data.info=TRUE,
+		sgp.target.scale.scores=TRUE,
+		plot.types=c("growthAchievementPlot", "studentGrowthPlot"),
+		sgPlot.demo.report=TRUE,
+		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4, GA_PLOTS=1, SG_PLOTS=1)))
+
+
+### Save results
+
+#save(WIDA_HI_SGP, file="Data/WIDA_HI_SGP.Rdata")
